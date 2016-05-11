87c451354e769d962f9cce6b455b4120dfcb691e
Oskar Stattin <stattin.o@gmail.com>
2016-05-11 15:25:54 +0200

    optimized hamiltonian and added a better heuristic for beta

--- Hamiltonian.m
+++ Hamiltonian.m
@@ -1,16 +1,11 @@
-function H = Hamiltonian( G, x)
+function H = Hamiltonian( G, x )
 %HAMILTONIAN(G,x) calculates the cost of the vector x on the adjacency matrix G
 %   G   (N x N) adjacency matrix for the underlying vertices
 %   x   (N x 1) color vector for vertices
-H = 0;
 [N, ~] = size(x);
-for i = 1:N
-   for j = i:N
-       if (G(i,j)==1 && x(i)==x(j))
-           H = H + 1;
-       end
-   end
-end
-
+X_mat = repmat(x,1,N);
+X_bool = real(X_mat' == X_mat);
+X = X_bool .* G;
+H = sum(sum(X))/2;
 end
 
