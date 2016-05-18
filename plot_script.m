b942adeaf47a7c03598a355e3e3b3e5d11c1b059
Ciwan Ceylan <ciwan_ceylan@hotmail.com>
2016-05-18 17:11:04 +0200

    plot finished

--- plot_script.m
+++ plot_script.m
@@ -1,11 +1,21 @@
 %%
 
+q_vec = [3,5,7];
+c_vec = 1:2:16;
 
-c           = 10;
-data.G      = generategraph(N,c);
-data.xinit  = randi(q,N,1);
-data.n      = 15000;
-data.q      = 5;
-data.temp   = 1;
+Hmin_res = zeros(length(q_vec),length(c_vec));
+iter_res = zeros(length(q_vec),length(c_vec));
+for j = 1:length(q_vec)
+    for i = 1:length(c_vec)
+        c           = c_vec(i);
+        data.G      = generategraph(N,c);
+        data.xinit  = randi(q,N,1);
+        data.n      = 15000;
+        data.q      = q_vec(j);
+        data.temp   = 1;
 
-[ Hmin, nrIterations ] = runMetopolis( data );
+        [ Hmin, nrIterations ] = runMetopolis(data);
+        Hmin_res(j,i) = Hmin;
+        iter_res(j,i) = nrIterations;
+    end
+end
